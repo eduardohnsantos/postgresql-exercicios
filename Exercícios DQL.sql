1a77e58d30e28b3d9856f9c6887ecc40d53ff410
Eduardo Henrique <eduardo.hnsantos@gmail.com>
2025-02-20 23:23:00 -0300

    Exercicios DQL

---
+++ Exercícios DQL.sql
@@ -0,0 +1,11 @@
+Exercícios de DQL
+Agora, para você treinar, aqui vão alguns desafios:
+
+🔍 Liste todos os livros com preço acima de R$ 35,00.
+📚 Mostre os títulos dos livros e o nome de seus autores.
+💰 Calcule o total de vendas (em reais) de cada livro.
+🎯 Liste os clientes que já compraram mais de 1 livro.
+📅 Mostre todas as vendas feitas nos últimos 30 dias.
+📊 Liste os autores e a quantidade de livros vendidos por cada um.
+🏆 Qual é o livro mais vendido?
+🌍 Liste os autores agrupados por país.
